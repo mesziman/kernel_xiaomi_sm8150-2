90b0a314f7ef0c4f71a8e221dd0c9ea12a058a69
Park Ju Hyung <qkrwngud825@gmail.com>
2019-07-16 03:42:50 +0900

    Revert "ARM: gic-v3: Log the IRQs in RTB before handling an IRQ"
    This reverts commit 5f0823d3f6524e9531be2d3cf1f11d7e2e68946d.
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    
    # Conflicts:
    #	drivers/irqchip/irq-gic-v3.c

--- drivers/irqchip/irq-gic-v3.c
+++ drivers/irqchip/irq-gic-v3.c
@@ -29,7 +29,6 @@
 #include <linux/percpu.h>
 #include <linux/slab.h>
 #include <linux/msm_rtb.h>
-#include <linux/wakeup_reason.h>
 
 #include <linux/irqchip.h>
 #include <linux/irqchip/arm-gic-common.h>
@@ -562,7 +561,6 @@ static asmlinkage void __exception_irq_entry gic_handle_irq(struct pt_regs *regs
 		if (likely(irqnr > 15 && irqnr < 1020) || irqnr >= 8192) {
 			int err;
 
-			uncached_logk(LOGK_IRQ, (void *)(uintptr_t)irqnr);
 			if (static_key_true(&supports_deactivate))
 				gic_write_eoir(irqnr);
 			else
@@ -583,7 +581,6 @@ static asmlinkage void __exception_irq_entry gic_handle_irq(struct pt_regs *regs
 			continue;
 		}
 		if (irqnr < 16) {
-			uncached_logk(LOGK_IRQ, (void *)(uintptr_t)irqnr);
 			gic_write_eoir(irqnr);
 			if (static_key_true(&supports_deactivate))
 				gic_write_dir(irqnr);
