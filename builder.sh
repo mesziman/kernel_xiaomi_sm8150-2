190e6ec1151708235a63ac5748059366aad0b3cd
mesziman <meszaros.gabor.10k@gmail.com>
2020-06-08 11:11:52 +0200

    add changelog

--- builder.sh
+++ builder.sh
@@ -55,4 +55,5 @@ cd $ANYKERNEL_DIR/
 zip -r9 $FINAL_ZIP * -x *.zip $FINAL_ZIP
 cp $FINAL_ZIP ${WERCKER_REPORT_ARTIFACTS_DIR}/
 curl -F chat_id="-1001477254593" -F document=@"$FINAL_ZIP" https://api.telegram.org/bot$BOT_API_KEY/sendDocument
+curl -s -X POST https://api.telegram.org/bot$BOT_API_KEY/sendMessage -d text="$(cat changelog.txt)" -d chat_id=@meszimankernel ;
 mv $FINAL_ZIP /pipeline/output/$FINAL_ZIP
