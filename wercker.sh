86a500ced992dac0263d56d2bc9acc1b3afd04ea
mesziman <meszaros.gabor.10k@gmail.com>
2020-06-08 10:49:46 +0200

    Build with GCC 9.3 again
    This reverts commit 82fafb5f0600e3a3e2bff176b705660fbdd7a519.
    
    # Conflicts:
    #	builder.sh
    #	wercker.sh

--- wercker.sh
+++ wercker.sh
@@ -6,6 +6,9 @@ apt-get remove -y clang;
 echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
 apt-get -qq install -y  xxd git flex libfl2 libomp-dev python libisl-dev git ccache automake bc lzop bison gperf build-essential zip curl zlib1g-dev libxml2-utils bzip2 libbz2-dev libbz2-1.0 libghc-bzlib-dev squashfs-tools pngcrush schedtool dpkg-dev liblz4-tool make optipng openssl libssl-dev > /dev/null ;
 export LOFASZ=$PWD;
-git clone --depth=1 https://github.com/kdrag0n/proton-clang /pipeline/build/root/toolchain/clang;
+
+git clone --depth=1 https://github.com/arter97/arm64-gcc /pipeline/build/root/toolchain/supergcc;
+git clone --depth=1 https://github.com/arter97/arm32-gcc /pipeline/build/root/toolchain/supergcc32;
+
 cd $LOFASZ
 bash builder.sh
