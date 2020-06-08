11b1a2366f9ac293b8429133edfbde2cf6691e5b
mesziman <meszaros.gabor.10k@gmail.com>
2020-06-08 10:49:46 +0200

    Build with GCC 9.3 again
    This reverts commit 82fafb5f0600e3a3e2bff176b705660fbdd7a519.
    
    # Conflicts:
    #	builder.sh
    #	wercker.sh

--- builder.sh
+++ builder.sh
@@ -23,6 +23,7 @@ export ARCH=arm64
 export KBUILD_BUILD_USER="mesziman"
 export KBUILD_BUILD_HOST="github"
 
+
 ls -l ${TOOLCHAINDIR}/clang
 echo "=========================debug============================================"
 echo " cc-namex: $(shell ${CC} -v 2>&1 )"
@@ -42,16 +43,71 @@ echo $ccname
 echo "=========================debug============================================"
 make clean && make mrproper
 make O=out -C $KERNEL_DIR cepheus_defconfig
-PATH=$TOOLCHAINDIR:$PATH make \
-ARCH=arm64 \
-CC=$TOOLCHAINDIR/clang \
-CROSS_COMPILE=$TOOLCHAINDIR/aarch64-linux-gnu- \
-CROSS_COMPILE_ARM32=$TOOLCHAINDIR/arm-linux-gnueabi- \
-AR=$TOOLCHAINDIR/llvm-ar \
-NM=$TOOLCHAINDIR/llvm-nm \
-OBJCOPY=$TOOLCHAINDIR/llvm-objcopy \
-OBJDUMP=$TOOLCHAINDIR/llvm-objdump \
-STRIP=$TOOLCHAINDIR/llvm-strip  O=out -C $KERNEL_DIR  -j$buildspeed  2>&1 | tee ${WERCKER_REPORT_ARTIFACTS_DIR}/errorlog.txt
+
+${KERNEL_DIR}/scripts/config --file $KERNEL_DIR/out/.config \
+    -d CGROUP_DEBUG \
+    -d CMA_DEBUGFS \
+    -d PM_DEBUG \
+    -d DEBUG_PAGEALLOC \
+    -d SLUB_DEBUG_PANIC_ON \
+    -d DEBUG_PAGEALLOC_ENABLE_DEFAULT \
+    -d DEBUG_OBJECTS \
+    -d DEBUG_OBJECTS_FREE \
+    -d DEBUG_OBJECTS_TIMERS \
+    -d DEBUG_OBJECTS_WORK \
+    -d DEBUG_OBJECTS_PERCPU_COUNTER \
+    -d DEBUG_KMEMLEAK \
+    -d DEBUG_KMEMLEAK_DEFAULT_OFF \
+    -d DEBUG_KMEMLEAK_EARLY_LOG_SIZE \
+    -d DEBUG_STACK_USAGE \
+    -d DEBUG_SPINLOCK \
+    -d DEBUG_MUTEXES \
+    -d DEBUG_ATOMIC_SLEEP \
+    -d DEBUG_SG \
+    -d DEBUG_NOTIFIERS \
+    -d DEBUG_CREDENTIALS \
+    -d LOCK_TORTURE_TEST \
+    -d RCU_TORTURE_TEST \
+    -d FAULT_INJECTION \
+    -d FAIL_PAGE_ALLOC \
+    -d FAULT_INJECTION_STACKTRACE_FILTER \
+    -d DEBUG_SECTION_MISMATCH \
+    -d DEBUG_MEMORY_INIT \
+    -d RMNET_DATA_DEBUG_PKT \
+    -d ESOC_DEBUG \
+    -d FHANDLE \
+    -d RD_BZIP2 \
+    -d RD_LZMA \
+    -d SYSFS_SYSCALL \
+    -d SLAB_FREELIST_RANDOM \
+    -d SLAB_FREELIST_HARDENED \
+    -d CMA_DEBUGFS \
+    -e HARDEN_BRANCH_PREDICTOR \
+    -d EFI \
+    -d L2TP_DEBUGFS \
+    -d REGMAP_ALLOW_WRITE_DEBUGFS \
+    -d CORESIGHT \
+    -d PAGE_POISONING \
+    -d QCOM_RTB \
+    -d BLK_DEV_IO_TRACE \
+    -d PREEMPTIRQ_EVENTS \
+    -d PREEMPT_TRACER \
+    -d IRQSOFF_TRACER \
+    -d PAGE_OWNER \
+    -d DRM_SDE_EVTLOG_DEBUG \
+    -d DRM_MSM_REGISTER_LOGGING \
+    -d MSM_SDE_ROTATOR_EVTLOG_DEBUG \
+    -d VIDEO_ADV_DEBUG \
+    -d IPU_DEBUG \
+    -d SPMI_MSM_PMIC_ARB_DEBUG \
+    -d WQ_WATCHDOG \
+    -d SCHED_STACK_END_CHECK \
+    -d LOCKUP_DETECTOR \
+    -d SOFTLOCKUP_DETECTOR \
+    -d MHI_DEBUG
+
+make -s O=out -C $KERNEL_DIR  -j$buildspeed ARCH=arm64 CROSS_COMPILE=${TOOLCHAINDIR}/bin/aarch64-elf- CROSS_COMPILE_ARM32=${TOOLCHAIN32}/bin/arm-eabi- | grep "error:"
+
 {
 cp $KERNEL_DIR/out/arch/arm64/boot/Image.gz-dtb $ANYKERNEL_DIR/
 } || {
