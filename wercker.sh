d7d1854b00be8856dd5c2223f2e3c0bd9fa636ab
mesziman <meszaros.gabor.10k@gmail.com>
2020-06-16 15:26:47 +0200

    install openssl

--- wercker.sh
+++ wercker.sh
@@ -3,7 +3,7 @@ dpkg --add-architecture amd64
 apt-get -qq update > /dev/null ;
 apt-get -qq install -y  dialog apt-utils > /dev/null ;
 echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
-apt-get -qq install -y  xxd git flex libfl2 libomp-dev binutils-arm-linux-gnueabi g++-multilib gcc-multilib binutils-aarch64-linux-gnu python libisl-dev git ccache automake bc lzop bison gperf build-essential zip curl zlib1g-dev  g++-multilib  libxml2-utils bzip2 libbz2-dev libbz2-1.0 libghc-bzlib-dev squashfs-tools pngcrush schedtool dpkg-dev liblz4-tool make optipng > /dev/null ;
+apt-get -qq install -y  xxd git flex libfl2 libomp-dev binutils-arm-linux-gnueabi g++-multilib gcc-multilib binutils-aarch64-linux-gnu python libisl-dev git ccache automake bc lzop bison gperf build-essential zip curl zlib1g-dev  g++-multilib  libxml2-utils bzip2 libbz2-dev libbz2-1.0 libghc-bzlib-dev squashfs-tools pngcrush schedtool dpkg-dev liblz4-tool make optipng openssl > /dev/null ;
 export LOFASZ=$PWD;
 git clone --depth=1 https://github.com/arter97/arm64-gcc /pipeline/build/root/toolchain/supergcc;
 git clone --depth=1 https://github.com/arter97/arm32-gcc /pipeline/build/root/toolchain/supergcc32;
