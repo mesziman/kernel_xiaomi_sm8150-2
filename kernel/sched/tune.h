810c3d28778224f975eb2cdec209ba9df2975564
mesziman <meszaros.gabor.10k@gmail.com>
2020-10-20 19:13:05 +0200

    xRevert "Revert "Revert "sched: Implement crucial task placement strategy"""
    This reverts commit 277e2ac58f4ea38f4c70e3800e251377ecab2fcd.

--- kernel/sched/tune.h
+++ kernel/sched/tune.h
@@ -15,7 +15,6 @@ struct target_nrg {
 int schedtune_cpu_boost(int cpu);
 int schedtune_task_boost(struct task_struct *tsk);
 int schedtune_task_boost_rcu_locked(struct task_struct *tsk);
-int schedtune_crucial(struct task_struct *tsk);
 
 int schedtune_prefer_idle(struct task_struct *tsk);
 
@@ -26,7 +25,6 @@ void schedtune_dequeue_task(struct task_struct *p, int cpu);
 
 #define schedtune_cpu_boost(cpu)  0
 #define schedtune_task_boost(tsk) 0
-#define schedtune_crucial(tsk)    0
 
 #define schedtune_prefer_idle(tsk) 0
 
