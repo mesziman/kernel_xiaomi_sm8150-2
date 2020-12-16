9bbb27adbe35b6e53d6a561089dec0d2dd5b09f9
mesziman <meszaros.gabor.10k@gmail.com>
2020-12-16 21:41:16 +0100

    Revert "compile with sdclang"
    This reverts commit cedde1f639034c31147c23fc5481790c4da4ab71.
    
    # Conflicts:
    #	builder.sh

--- builder.sh
+++ builder.sh
@@ -5,9 +5,8 @@ git submodule update scripts
 KERNEL_DIR=$PWD
 ANYKERNEL_DIR=$KERNEL_DIR/AnyKernel3
 CCACHEDIR=../CCACHE/cepheus
-TOOLCHAINDIR=/pipeline/build/root/toolchain/sdclang
+TOOLCHAINDIR=/pipeline/build/root/toolchain/supergcc
 TOOLCHAIN32=/pipeline/build/root/toolchain/supergcc32
-TOOLCHAIN64=/pipeline/build/root/toolchain/supergcc64
 DATE=$(date +"%d%m%Y")
 KERNEL_NAME="mesziman"
 DEVICE="-cepheus-R11-TESTBUILD-"
@@ -23,29 +22,16 @@ export ARCH=arm64
 export KBUILD_BUILD_USER="mesziman"
 export KBUILD_BUILD_HOST="github"
 
-export PATH="${TOOLCHAINDIR}/bin:${TOOLCHAIN64}/bin:${TOOLCHAIN32}/bin:${PATH}"
+export PATH="${TOOLCHAINDIR}/bin:${TOOLCHAIN32}/bin:${PATH}"
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${TOOLCHAINDIR}/lib"
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${TOOLCHAIN32}/lib"
-
 git config --global user.email "you@example.com"
 git config --global user.name "Your Name"
-git revert c29f49fee539 --no-edit;
 
-echo "====which lld===="
-which lld
-echo "====which lld===="
+git revert c29f49fee539 --no-edit;
 make clean && make mrproper
 make O=out -C $KERNEL_DIR cepheus_defconfig
-make ARCH=arm64 \
-CC=clang \
-LD=ld.lld \
-CROSS_COMPILE=$TOOLCHAIN64/bin/aarch64-elf-  \
-CROSS_COMPILE_ARM32=$TOOLCHAIN32/bin/arm-eabi- \
-AR=$TOOLCHAINDIR/bin/llvm-ar \
-NM=$TOOLCHAINDIR/bin/llvm-nm \
-OBJCOPY=$TOOLCHAINDIR/bin/llvm-objcopy \
-OBJDUMP=$TOOLCHAINDIR/bin/llvm-objdump \
-STRIP=$TOOLCHAINDIR/bin/llvm-strip -s O=out -C $KERNEL_DIR -j$buildspeed 2>&1 | tee ${WERCKER_REPORT_ARTIFACTS_DIR}/errorlog.txt
+make -s O=out -C $KERNEL_DIR  -j$buildspeed ARCH=arm64 CROSS_COMPILE=${TOOLCHAINDIR}/bin/aarch64-elf- CROSS_COMPILE_ARM32=${TOOLCHAIN32}/bin/arm-eabi- 2>&1 | tee ${WERCKER_REPORT_ARTIFACTS_DIR}/errorlog.txt
 {
 cp $KERNEL_DIR/out/arch/arm64/boot/Image.gz-dtb $ANYKERNEL_DIR/
 } || {
