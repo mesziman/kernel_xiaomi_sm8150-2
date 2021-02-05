7920194395d171760e1ec8745f1831537584aafb
mesziman <meszaros.gabor.10k@gmail.com>
2021-02-05 10:24:46 +0100

    get wget

--- wercker.sh
+++ wercker.sh
@@ -4,7 +4,7 @@ apt-get -qq update > /dev/null ;
 apt-get -qq install -y  dialog apt-utils > /dev/null ;
 apt-get remove -y clang;
 echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
-apt-get -qq install -y  xxd git  binutils-arm-linux-gnueabi g++-multilib gcc-multilib binutils-aarch64-linux-gnu flex libfl2 libomp-dev python libisl-dev git ccache automake bc lzop bison gperf build-essential zip curl zlib1g-dev libxml2-utils bzip2 libbz2-dev libbz2-1.0 libghc-bzlib-dev squashfs-tools pngcrush schedtool dpkg-dev liblz4-tool make optipng openssl libssl-dev > /dev/null ;
+apt-get -qq install -y  xxd git wget binutils-arm-linux-gnueabi g++-multilib gcc-multilib binutils-aarch64-linux-gnu flex libfl2 libomp-dev python libisl-dev git ccache automake bc lzop bison gperf build-essential zip curl zlib1g-dev libxml2-utils bzip2 libbz2-dev libbz2-1.0 libghc-bzlib-dev squashfs-tools pngcrush schedtool dpkg-dev liblz4-tool make optipng openssl libssl-dev > /dev/null ;
 export LOFASZ=$PWD;
 
 # git clone --depth=1 https://github.com/mvaisakh/gcc-arm64 /pipeline/build/root/toolchain/supergcc;
